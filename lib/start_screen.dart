5429201419f5105d66e66b732e36400d964f3981
Christo Johnson <christojohnson.dev@gmail.com>
2026-06-14 19:16:20 +0530

    feat: Questions and answers added.

--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
 
   @override
   Widget build(context) {
@@ -17,14 +20,15 @@ class StartScreen extends StatelessWidget {
           SizedBox(height: 80),
           Text(
             "Learn Flutter the fun way!",
-            style: TextStyle(
-              color: Color.fromARGB(255, 237, 223, 252),
+            style: GoogleFonts.lato(
+              color: const Color.fromARGB(255, 237, 223, 252),
               fontSize: 24,
+              fontWeight: FontWeight.bold,
             ),
           ),
           const SizedBox(height: 30),
           OutlinedButton.icon(
-            onPressed: () {},
+            onPressed: startQuiz,
             style: OutlinedButton.styleFrom(
               foregroundColor: Colors.white,
             ),
